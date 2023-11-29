5cd20d938f71a03a8eb4de3e83318d9eb50313a5
Glitch (glitch-hello-website) <none>
2023-11-29 18:34:10 +0000

    🏥🎀 Checkpoint ./.glitch-assets:62665635/1113 ./script.js:62665635/12

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let img;
 
 function preload() {
     classifier = ml5.imageClassifier('MobileNet');
-    img1 = loadImage('img/shark.jpg');
-    img2 = loadImage('img/moth.jpg');
-    img3 = loadImage('img/tree.jpg');
+    img1 = loadImage('shark.jpg');
+    img2 = loadImage('moth.jpg');
+    img3 = loadImage('tree.jpg');
 }
 
 
